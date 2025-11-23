683552fb501d87a45593990238b5f69d3a070517
Eric <erichansson.united@gmail.com>
2025-11-23 14:58:39 -0500

    Updated components for the Home page, just to have something simple.

---
+++ src/data/HomeCards.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        title: "Books and Notes",
+        url: "assets/book_notes.jpg",
+        link_url: "/books"
+    },
+    {
+        title: "Account",
+        url: "assets/user-management.png",
+        link_url: "/dashboard"
+    }
+] 
