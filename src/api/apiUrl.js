9bfdcb63e88eb6bad279f7f13f0a92cc9269d408
Eric <erichansson.united@gmail.com>
2025-11-24 11:06:00 -0500

    Removed leading '/'.

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -1,1 +1,1 @@
-export const apiUrl = process.env.NODE_ENV === "development" ? "http://localhost:5050" : "https://arcana-backend.vercel.app/";
+export const apiUrl = process.env.NODE_ENV === "development" ? "http://localhost:5050" : "https://arcana-backend.vercel.app";
