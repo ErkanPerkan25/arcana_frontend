debfab6e55f897a5c661b2b7ead5902f7337f83f
Eric <erichansson.united@gmail.com>
2025-11-30 13:01:27 -0500

    Fixed an issue with the Search component

--- src/components/SearchComp.jsx
+++ src/components/SearchComp.jsx
@@ -92,7 +92,7 @@ function SearchComp({hidVar, infoBack}){
                 />
 
                 <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                    {!data ? "" :
+                    {!data ? "Loading..." :
                         data.map((item, index) =>(
                         <div
                             key={index}
